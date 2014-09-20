968ad2ae62b1a0ab32cf878c091bd28bae01d166
Adnan Pirota <adnanpirota2011@gmail.com>
2014-09-20 21:29:29 +0200

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.0'
 
 
 group :test do
+  gem 'factory_girl_rails','4.2.0'
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name	"Ajan Pirota"
+    email	"apirota@hotmail.com"
+    password	"MY4zrg22"
+    password_confirmation	"MY4zrg22"
+  end
+end
